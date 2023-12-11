c014ca6f2dbe9dbae950e14add7a9e5f89495335
eugiv <eugene.ivanov@ymail.com>
2023-12-11 17:16:08 +0300

    added more

---
+++ orm_provider.py
@@ -0,0 +1,30 @@
+import sqlalchemy as sq
+from sshtunnel import SSHTunnelForwarder
+import json
+
+class DBConnector:
+    def __init__(self, sens_file: str, host: str, database_port: int,
+                 ssh_user: str, ssh_port: int, database_user: str, database: str):
+        self.sens_file = sens_file
+        self.host = host
+        self.database_port = database_port
+        self.ssh_user = ssh_user
+        self.ssh_port = ssh_port
+        self.database_user = database_user
+        self.database = database
+
+    def connection(self):
+        with open(self.sens_file) as f:
+            file = json.loads(f.read())
+            aws_dns = file['aws_dns']
+            postgres_password = file['password']
+
+        ec2_tunnel = SSHTunnelForwarder(
+            (aws_dns, self.ssh_port),
+            ssh_host_key=None,
+            ssh_username=self.ssh_user,
+            ssh_password=None,
+            ssh_pkey='/Users/eugene_ivanov/AWS/EC2/linux_server/eug_linux_server_key.pem',
+            remote_bind_address=(self.host, self.database_port))
+
+        ec2_tunnel.start()
